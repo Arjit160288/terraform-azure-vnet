5ae36c3f65afeeb38a27496dfbfc668745f215c4
Arjit160288 <72435449+Arjit160288@users.noreply.github.com>
2025-06-08 00:42:02 +0530

    Create main.tf

---
+++ Modules/main.tf
@@ -0,0 +1,7 @@
+
+module "vnet" {
+  source              = "./modules/terraform-azure-vnet"
+  vnet_name           = "my-vnet"
+  location            = "East US"
+  resource_group_name = "my-resource-group"
+  vnet_address_space  = "10.0.0.0/16"

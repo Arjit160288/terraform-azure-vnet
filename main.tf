61344b68066ef2258dbbe2d836e79b69316dbbd9
Arjit160288 <72435449+Arjit160288@users.noreply.github.com>
2025-06-07 23:56:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,20 +10,24 @@ terraform {
 provider "azurerm" {
   features {}
 }
+resource "azurerm_resource_group" "example" {
+  name                = "${var.resource_group}"
+  location            = "${var.location}"
+}
 
 resource "azurerm_virtual_network" "this" {
-  name                = var.vnet_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = var.address_space
+  name                = "${var.vnet}"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  address_space       = ["${var.vnet_address}"]
   tags                = var.tags
 }
 
 resource "azurerm_subnet" "this" {
   for_each = { for subnet in var.subnets : subnet.name => subnet }
 
-  name                 = each.value.name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = each.value.address_prefixes
+  name                 = "testsubnet"
+  resource_group_name  = "${azurerm_resource_group.example.name}"
+  virtual_network_name = "${azurerm_virtual_network.this.name}"
+  address_prefixes     = "10.0.1.0/24"
 }

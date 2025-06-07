650dcd64fc14bf05d09d2c8ed95152b1bb74cc42
Arjit160288 <72435449+Arjit160288@users.noreply.github.com>
2025-06-08 00:43:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,7 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-resource "azurerm_resource_group" "example" {
-  name                = "${var.resource_group}"
-  location            = "${var.location}"
-}
-
-resource "azurerm_virtual_network" "this" {
-  name                = "${var.vnet}"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  address_space       = ["${var.vnet_address}"]
-  tags                = var.tags
-}
-
-resource "azurerm_subnet" "this" {
-  for_each = { for subnet in var.subnets : subnet.name => subnet }
-
-  name                 = "${var.subnets[count.index]}"
-  resource_group_name  = "${azurerm_resource_group.example.name}"
-  virtual_network_name = "${azurerm_virtual_network.this.name}"
-  address_prefixes     = ["${var.subnet_address_prefix}"]
-  count                = "${length{var.subnets}}"
+module "vnet" {
+  source              = "./modules/terraform-azure-vnet"
+  vnet_name           = "my-vnet"
+  location            = "East US"
+  resource_group_name = "my-resource-group"
+  vnet_address_space  = "10.0.0.0/16"
 }

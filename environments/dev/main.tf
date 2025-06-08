d9ca61a6d28e5c08d8386fd55a6be5495b4ce2a1
Arjit160288 <72435449+Arjit160288@users.noreply.github.com>
2025-06-08 12:36:13 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -10,3 +10,30 @@ module "Modules" {
     project     = "vnet-module"
   }
 }
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "dev-linux-vm"
+  resource_group_name = "my_resource-group"
+  location            = "East US"
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic.id,
+  ]
+
+  admin_password = "XXXXXXXX"
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}

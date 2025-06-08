b45f1d7e985a42d438c90c87d48eb0f9d6fba01d
Arjit160288 <72435449+Arjit160288@users.noreply.github.com>
2025-06-08 12:27:04 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,1 +1,12 @@
+module "Modules" {
+  source              = "./Modules"
+  vnet_name           = "my_vnet"
+  location            = "East US"
+  resource_group_name = "my_resource-group"
+  vnet_address        = "10.0.0.0/16"
 
+  tags = {
+    environment = "dev"
+    project     = "vnet-module"
+  }
+}
